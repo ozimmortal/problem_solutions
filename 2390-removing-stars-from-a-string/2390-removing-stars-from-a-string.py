127664054567461188697fc9313b6b4b7551d152
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-03-09 19:00:50 +0300

    95 ms (49.07%) 20.4 MB (91.42%) 03-09-2026 Medium

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+
+        for c in s:
+            if stack and  c == "*":
+                stack.pop()
+                continue
+            stack.append(c)
+        return "".join(stack)
+        
