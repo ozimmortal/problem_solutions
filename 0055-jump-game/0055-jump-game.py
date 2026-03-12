0e28f2bae8bf4260c2b4e3eb6ebe9d18a83e1166
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-03-12 21:32:42 +0300

    12 ms (85.97%) 20.5 MB (30.62%) 03-12-2026 Medium

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        g = len(nums) - 1
+
+        for i in range(len(nums) - 1 , -1, -1):
+            if i + nums[i] >= g:
+                g = i
+        return False if g != 0 else True
+        
