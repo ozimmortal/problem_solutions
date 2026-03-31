62a8ae39e09a9db69b8980ce60a86c05c426c40c
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-03-31 19:16:39 +0300

    N/A (0%) N/A (0%) 03-31-2026 Easy

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,9 +1,27 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 0 or x == 1:
-            return x
-        l = math.log(x,2)
-        c = 0.5 * l
-        res = 2 ** c
-        return math.ceil(res) if math.ceil(res) - res < 0.00000001  else int(res)
+        # if x == 0 or x == 1:
+        #     return x
+        # l = math.log(x,2)
+        # c = 0.5 * l
+        # res = 2 ** c
+        # return math.ceil(res) if math.ceil(res) - res < 0.00000001  else int(res)
+
+        l = 0
+        r = x
+        ans = 0
+
+        while l <= r:
+
+            m = (l + r) // 2
+
+            if m * m == x:
+                ans = m
+                break
+            elif m * m  > x:
+                r = m -1
+            else:
+                ans = m
+                l = m + 1
+        return ans
         
