719cec5aedefd78012bc6be1a816b9fcbd78008d
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-04-01 12:55:06 +0300

    0 ms (100%) 21.1 MB (33.7%) 04-01-2026 Medium

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        nodes = []
+
+        def dfs(node):
+            if not node : return 
+
+            dfs(node.left)
+            nodes.append(node.val)
+            dfs(node.right)
+        
+        dfs(root)
+
+        for i in range(1,len(nodes)):
+            if nodes[i - 1] >= nodes[i]:
+                return False
+        return True
