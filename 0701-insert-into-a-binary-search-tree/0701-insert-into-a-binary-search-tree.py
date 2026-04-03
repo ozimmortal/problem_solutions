43433d6f098715386b57d088385912eda622e78d
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-04-03 21:45:08 +0300

    4 ms (19.63%) 21.1 MB (89.96%) 04-03-2026 Medium

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        temp = TreeNode(val)
+        def dfs(node):
+            nonlocal temp
+
+            if not node: return temp
+
+            if node.val > val:
+                node.left = dfs(node.left)
+            if node.val < val:
+                node.right = dfs(node.right)
+
+            return node
+        
+        return dfs(root)
+        
