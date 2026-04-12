36363f0b74c21893609ee61f3ec85e830abf20fb
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-04-12 10:21:16 +0300

    109 ms (51.84%) 61.3 MB (87.32%) 04-12-2026 Medium

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,16 +1,18 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
-        def solve(s , comb):
-
-            if  len(comb) == k:
-                res.append(comb[:])
+        
+        def backtrack(curr , i):
+            if len(curr) == k:
+                res.append(curr[:])
                 return
-            for i in range(s,n+1):
-                comb.append(i)
-                solve(i+1,comb)
-                comb.pop()
-        solve(1,[])
+            
+            for j in range(i , n + 1):
+                curr.append(j)
+                backtrack(curr , j + 1)
+                curr.pop()
+        backtrack([] , 1)
+
         return res
 
 
