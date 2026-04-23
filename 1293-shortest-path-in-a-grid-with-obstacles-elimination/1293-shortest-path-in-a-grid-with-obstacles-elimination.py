12c8d1786dc71838094b4e1531b9447bbe10b655
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-04-23 19:15:34 +0300

    339 ms (38.08%) 28.4 MB (29.47%) 04-23-2026 Hard

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -0,0 +1,35 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        
+        #               x   y   s   v->k
+        queue = deque([(0 , 0 , 0 , k)])
+        seen = {(0 , 0 , k)}
+        directions = [(1,0) , (0, 1) , (-1 , 0), (0, -1)]
+        m , n = len(grid) , len(grid[0])
+
+        def valid(x , y):
+            return 0 <= x < m and 0 <= y < n
+
+        while queue:
+            x , y , s , v = queue.popleft()
+
+            if x == m -1 and y == n-1:
+               return s
+            
+            for dx , dy in directions:
+                nx , ny = x + dx , y + dy
+                if valid(nx , ny) and (nx , ny , v) not in seen:
+
+                    if grid[nx][ny] == 0:
+                        queue.append((nx , ny , s + 1 , v))
+                        seen.add((nx ,ny , v))
+                        continue
+
+                    if grid[nx][ny] == 1 and v > 0:
+                        queue.append((nx , ny , s + 1 , v - 1))
+                        seen.add((nx ,ny , v))
+                        continue
+
+                    
+        return -1
+                        
