596007104b093fbf408264d937324268315e38c0
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-05-04 22:35:41 +0300

    14 ms (6.21%) 56.4 MB (46.96%) 05-04-2026 Easy

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -6,19 +6,15 @@
 var canPlaceFlowers = function(flowerbed, n) {
     let canBePlanted = 0;
     let j =0 , l = flowerbed.length
-    while ( j < n){
-        for(let i=0; i<l; i++){
+    for(let i=0; i<l; i++){
             const left = i - 1,  right = i + 1; 
             
             if((flowerbed[i]==0) && (left < 0 || (left>=0 && flowerbed[left]==0)) && (right >= l || (right<l && flowerbed[right]==0))){
                 canBePlanted++;
                 flowerbed[i] = 1
-                break;
             }
             
         }
-        j++;
-    }
     console.log(flowerbed)
-    return canBePlanted === n
+    return canBePlanted >= n
 };
