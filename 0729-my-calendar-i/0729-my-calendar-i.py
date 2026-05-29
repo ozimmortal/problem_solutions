9c530f87af5e7f47252538c6dd0f91ad442f9d7d
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-05-29 22:57:43 +0300

    187 ms (41.34%) 20 MB (97.09%) 05-29-2026 Medium

---
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -0,0 +1,19 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.events = []
+        
+
+    def book(self, startTime: int, endTime: int) -> bool:
+
+        for s , e in self.events:
+            if not (endTime <= s or e <= startTime):
+                return False
+        self.events.append([startTime, endTime])
+        return True
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
