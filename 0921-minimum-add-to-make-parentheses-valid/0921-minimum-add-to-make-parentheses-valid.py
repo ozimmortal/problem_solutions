fcaa6ae153d888ce5385a4f0773f1730b14a4f78
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-05-30 22:25:18 +0300

    0 ms (100%) 19.5 MB (15.21%) 05-30-2026 Medium

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = []
+        for ch in s:
+
+            if ch == "(":
+                stack.append(ch)
+            else:
+                if stack and stack[-1] == "(":
+                    stack.pop()
+                else:
+                    stack.append(ch)
+        return len(stack)
