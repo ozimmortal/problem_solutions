3889e3646a7aa11704476f43966a4007b7a24e46
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-06-12 23:14:50 +0300

    7 ms (54.26%) 20.2 MB (16.48%) 06-12-2026 Medium

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,17 +1,15 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        queue = deque([(0 , nums[0] , 0)])
-        seen = {0}
-        while queue:
-            i , j , s = queue.popleft()
+        l , r = 0 , 0
+        res = 0 
+        while r < n -1:
+            farthest = 0
+            for i in range(l , r + 1):
+                farthest = max(farthest , i + nums[i])
             
-            if i == n - 1:
-                return s
+            l = r + 1
+            r = farthest
+            res += 1
+        return res
             
-            for j in range(1 , j+1):
-                k = i + j
-                if i + j < n and k not in seen:
-                    queue.append((k, nums[k] , s + 1))
-                    seen.add(k)
-        
