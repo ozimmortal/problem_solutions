8ad3387453e660a39376c3c9c1505b07a0ad0a7c
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-06-19 15:40:36 +0300

    11 ms (21.85%) 20.4 MB (54.97%) 06-19-2026 Medium

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,25 +1,33 @@
+class UF:
+    def __init__(self , n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+        self.count = n
+    
+    def find(self,x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self , x , y):
+        rx , ry = self.find(x) , self.find(y)
+        
+        if rx == ry: return
+        if self.size[rx] < self.size[ry]:
+            rx ,ry = ry , rx
+        
+        self.parent[ry] = rx
+        self.size[rx] += self.size[ry]
+        self.count -= 1
+    
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        
-        graph = defaultdict(list)
         n = len(isConnected)
+        uf = UF(n)
+
         for i in range(n):
             for j in range(n):
                 if isConnected[i][j] == 1:
-                    graph[i].append(j)
-        
-        def dfs(node):
-            arr = graph[node]
-            for n in arr:
-                if n not in seen:
-                    seen.add(n)
-                    dfs(n)
-        seen =set()
-        res = 0
-
-        for i in range(n):
-            if i not in seen:
-                dfs(i)
-                res += 1
-        return res
-            
+                    uf.union(i , j)
+                    
+        return uf.count         
