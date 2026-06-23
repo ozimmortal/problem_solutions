93f29f9adfab87ab0bfde0dcaa54f8a5a9b16c49
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-06-23 17:48:57 +0300

    18 ms (40.35%) 21.3 MB (95.62%) 06-23-2026 Hard

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,18 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        
+        n = len(ratings)
+        c = [1] * n
+
+        for i in range(1 , n):
+            if ratings[i] > ratings[i - 1]:
+                c[i] = c[i - 1] + 1
+        
+        for i in range(n - 2 , -1 , -1):
+            if ratings[i] > ratings[i + 1] and c[i] <= c[i + 1]:
+                c[i] = c[i + 1] + 1
+                
+        
+        print(c)
+        return sum(c)
+
