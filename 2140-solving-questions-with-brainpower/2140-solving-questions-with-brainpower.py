129d0a8b51b1948f4a012afe58d15125d8d9a532
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-06-28 15:32:17 +0300

    151 ms (17.26%) 74 MB (17.96%) 06-28-2026 Medium

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,12 +1,16 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         
-        @cache
+        memo ={}
         def dp(i):
+            if i in memo:
+                return memo[i]
+
             if i >= len(questions):
                 return 0
             point , skip = questions[i]
 
-            return max(dp(i + 1) , dp(i + skip + 1) + point)
+            memo[i] = max(dp(i + 1) , dp(i + skip + 1) + point)
+            return memo[i]
 
         return dp(0)
