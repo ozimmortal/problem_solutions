08f3d6ab74b294003dc00b5900febce3778881b7
Oliyad Zelalem Abdena <78490326+ozimmortal@users.noreply.github.com>
2026-06-29 16:36:27 +0300

    683 ms (5.11%) 218.9 MB (12.53%) 06-29-2026 Medium

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -4,19 +4,22 @@ class Solution:
         # skip
         # buy 
         
-        @cache
+        memo = {}
         def dp(i , h):
 
             if i == len(prices):
                 return 0
-
+            if (i , h) in memo:
+                return memo[(i , h)]
+                
             res = dp(i + 1 , h)
             if h:
                 res = max(res , prices[i] + dp(i + 1 , False) )
             else:
                 res = max(res , -prices[i] - fee + dp(i + 1 , True))
             
-            return res
+            memo[(i , h)] = res
+            return memo[(i, h)]
         
 
         return dp(0 , False)
