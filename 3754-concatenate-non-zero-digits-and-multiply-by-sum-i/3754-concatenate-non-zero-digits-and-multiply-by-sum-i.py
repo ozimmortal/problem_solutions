7bd3c565d9c294c62ba4bd5501c425792af3eb94
Oliyad Zelalem Abdena <oliyad.dev@gmail.com>
2026-07-07 17:19:15 +0300

    0 ms (100%) 19.3 MB (51.25%) 07-07-2026 Easy

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -2,9 +2,7 @@ class Solution:
     def sumAndMultiply(self, n: int) -> int:
         
         mul= 1
-        x = 0
-        su = 0
-
+        x , su = 0 , 0
         while n > 0:
             rem = n % 10
             if rem != 0:
