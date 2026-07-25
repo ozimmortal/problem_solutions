2e0bff57edb38fbbccfb75a9d188c429aa6e6b8a
Oliyad Zelalem Abdena <oliyad.dev@gmail.com>
2026-07-25 10:45:57 +0300

    0 ms (100%) 19.2 MB (84.08%) 07-25-2026 Easy

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -2,42 +2,48 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         
         rem = 0
-        res = ""
+        res = []
 
         p1 , p2 = len(a) -1 , len(b) - 1
         while p1 > -1 and p2 > -1:
             ch1 , ch2 = a[p1] , b[p2]
             t = int(ch1) + int(ch2) + rem
+            s = ""
             if t <= 1:
-                res += str(t)
+                s = str(t)
                 rem = 0
             else:
                 rem = 1
-                res += "0" if t == 2 else "1"
+                s = "0" if t == 2 else "1"
+            res.append(s)
             p1 -= 1
             p2 -= 1
         
         while p1 > -1:
             t = int(a[p1]) + rem
+            s = ""
             if t <= 1:
-                res += str(t)
+                s= str(t)
                 rem = 0
             else:
                 rem = 1
-                res += "0" if t == 2 else "1"
+                s= "0" if t == 2 else "1"
+            res.append(s)
             p1 -=1
         
         while p2 > -1:
             t = int(b[p2]) + rem
+            s = ""
             if t <= 1:
-                res += str(t)
+                s= str(t)
                 rem = 0
             else:
                 rem = 1
-                res += "0" if t == 2 else "1"
+                s= "0" if t == 2 else "1"
+            res.append(s)
             p2 -=1
 
         if rem:
-            res += "1"
-        return res[::-1]
+            res.append("1")
+        return "".join(res[::-1])
 
