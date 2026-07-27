eb57a5de3309e47c9338842cc49a4d58cfb3c29b
Oliyad Zelalem Abdena <oliyad.dev@gmail.com>
2026-07-27 13:06:22 +0300

    935 ms (11.09%) 19.3 MB (42.04%) 07-27-2026 Medium

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        MOD = 10 ** 9 + 7
+
+        ans = 0
+        for i in range(1 , n + 1):
+            k = floor(log(i , 2)) + 1
+            ans = ((ans << k ) + i) % MOD
+        return ans
+        
