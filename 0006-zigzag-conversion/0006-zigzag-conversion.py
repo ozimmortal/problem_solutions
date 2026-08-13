cd1ffbd4613245d4de336a1559e0bce14b296ecb
Oliyad Zelalem Abdena <oliyad.dev@gmail.com>
2026-08-13 20:56:15 +0300

    12 ms (35.49%) 19.2 MB (96.07%) 08-13-2026 Medium

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,14 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1: return s
+
+        l = len(s)
+        res = ""
+        for r in range(numRows):
+            incr = (numRows - 1) * 2
+            for i in range(r , l, incr):
+                res += s[i]
+                if 0 < r < numRows - 1 and i + incr - 2 * r < l:
+                    res += s[i + incr - 2 * r]
+        
+        return res
