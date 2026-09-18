4cc457baa0303d37baf8b5adf9ddade0a911e2bd
Oliyad Zelalem Abdena <oliyad.dev@gmail.com>
2026-09-18 12:56:22 +0300

    163 ms (16.63%) 53.5 MB (66.23%) 09-18-2026 Medium

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,22 +1,16 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort()
-        n = 0
-        temp = points[0]
-
-        for i in range(1,len(points)):
-            # check for intersection 
-
-            if points[i][0] <=  temp[1]:  
-                #  this means they intersect 
-                # if they intersect update the temp variable
-                temp = [points[i][0] , min(temp[1],points[i][1])]
-            else:
-                # if they dont intersect
-                n += 1
-                temp = points[i]
-
-        return n + 1
-
-
         
+        points.sort()
+
+        last = points[0]
+        res = 0
+
+        for i in range(1 , len(points)):
+            if points[i][0] <= last[1]:
+                last[1] = min(last[1], points[i][1])
+            else:
+                res += 1
+                last = points[i]
+        
+        return res + 1
